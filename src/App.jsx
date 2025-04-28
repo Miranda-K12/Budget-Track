151ac9537e928e96616d1b272964457425feba25
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-04-28 17:52:14 +0400

    center content

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LogInForm from './Components/LogIn/LogIn';  
-import Layout from './Components/Layout/Layout';  
-import Dashboard from './Components/Dashboard/Dashboard';  
-import Incomes from './Components/Incomes/Incomes'; 
-import Expenses from './Components/Expenses/Expenses'; 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LogInForm from "./Components/LogIn/LogIn";
+import Layout from "./Components/Layout/Layout";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Incomes from "./Components/Incomes/Incomes";
+import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} /> 
-        <Route path="/login" element={<LogInForm />} /> 
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LogInForm />} />
         <Route element={<Layout />}>
-          <Route path="dashboard" element={<Dashboard />} />  
-          <Route path="incomes" element={<Incomes />} />  
-          <Route path="expenses" element={<Expenses />} />  
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="incomes" element={<Incomes />} />
+          <Route path="expenses" element={<Expenses />} />
         </Route>
       </Routes>
     </Router>
@@ -24,7 +29,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
